9343b09c28c94f36cf927711770fccc3ffd0cd89
webcrawlers_br <git@webcrawlers.com.br>
2025-02-25 23:04:38 -0300

    primeiras lições

---
+++ aula01.py
@@ -0,0 +1,17 @@
+# declarando variável como ponto flutuante (decimal)
+taxa_importacao = 0.35
+
+# declarando variável como inteiro
+quantidade_comprada = 10
+
+# valor do produto
+valor_produto = 100
+
+# nome do produto
+nome_produto = 'Vibrador 1000 RPM para velha gorda'
+
+# valor do imposto
+valor_imposto = (valor_produto * quantidade_comprada) * taxa_importacao
+
+print(valor_imposto)
+
