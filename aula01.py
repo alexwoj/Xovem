a43211b40da3c1a4d11ab6ea47b0232a30472d2a
webcrawlers_br <git@webcrawlers.com.br>
2025-02-25 23:26:31 -0300

    desafio

--- aula01.py
+++ aula01.py
@@ -10,8 +10,29 @@ valor_produto = 100
 # nome do produto
 nome_produto = 'Vibrador 1000 RPM para velha gorda'
 
-# valor do imposto
-valor_imposto = (valor_produto * quantidade_comprada) * taxa_importacao
+# valor bruto
+valor_bruto = quantidade_comprada * valor_produto
 
-print(valor_imposto)
+# taxa de desconto
+taxa_desconto = 0.8
+
+# valor do imposto
+valor_imposto = valor_bruto * taxa_importacao
+
+# Desafio:
+#
+# Declarar as seguintes variáveis, de acordo com as melhores práticas do Python (snake_case), descrevendo a variável
+# de acordo com o que ela representa
+#
+# 1) Valor total da compra (bruto), sem impostos
+# 2) Valor total da compra (líquido), com impostos
+# 3) Valor total da compra, com desconto e impostos
+# 4) Dar um print nestas variáveis
+#
+# Sinal de multiplicação: *
+# Sinal de divisão:  /
+# Numero decimal (composto)  usa ponto, exemplo: 0.5
+# Soma: 1 + 1
+# Subtrai: 3 - 2
+
 
