4482cb7be029e95c7027c99cb46bfcb6d262de63
webcrawlers_br <git@webcrawlers.com.br>
2025-03-13 22:30:48 -0300

    exercicios para o xovem

---
+++ instrucoes.py
@@ -0,0 +1,10 @@
+# O xovem deverá:
+#
+# - Criar um arquivo chamado imposto.py onde deverá executar a rotina dos seguintes cálculos
+#   - declarar valor do produto
+#   - declarar taxa de imposto
+#   - declara desconto
+#   - calcular valor com desconto
+#   - calcular valor sem desconto
+#   - deve aplicar o imposto nos valores acima
+
