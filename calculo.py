d11c15497bcbb7b83a25d72b8a3369b13de2aaa3
Xovem <victfrs@gmail.com>
2025-03-13 22:49:18 -0300

    arquivo de valor total e com imposto

---
+++ calculo.py
@@ -0,0 +1,9 @@
+produto = 'marmitex'
+valor_produto = 35
+imposto_produto = 13.5
+desconto_produto = 0.8
+#
+valor_com_imposto = valor_produto + imposto_produto
+valor_com_desconto =  valor_com_imposto - desconto_produto
+print(valor_com_desconto, valor_com_imposto)
+
